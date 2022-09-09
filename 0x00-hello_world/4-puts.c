67eb2ec19d9669e8bd10b28c0a11466cc2864565
Sara <24sara.abdi@gmail.com>
2022-09-08 17:17:34 -0700

    hello_world

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+*main - print the string in the put function
+*
+* Description: using the main function
+* this program prints "Programming is like building a multilingual puzzle"
+* Return: 0
+*/
+int main(void)
+{
+ puts("\"Programming is like building a multilingual puzzle");
+ Return (0);
+}
+
+root@c4d143ede6f3:/alx-low_level_programming/0x00-hello_world#
