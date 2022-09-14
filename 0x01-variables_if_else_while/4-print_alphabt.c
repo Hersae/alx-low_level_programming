edee03d4c3ac16b0cec94f03cdc18352a019da16
Sara <24sara.abdi@gmail.com>
2022-09-14 15:03:57 -0700

    exceot q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main 
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	print(*["%c % a for a in range(ord('a'), ord('z')+1) if a not in (ord('q'), ord('e')], sep = '', end = '');
+			return (0)
+}
