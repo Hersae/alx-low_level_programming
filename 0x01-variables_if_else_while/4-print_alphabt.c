04937b4c0a832300953008731404e75a452ec857
Sara <24sara.abdi@gmail.com>
2022-09-14 15:13:33 -0700

    4-print_alphabt

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,6 +7,14 @@
  */
 int main(void)
 {
-	print(*["%c % a for a in range(ord('a'), ord('z')+1) if a not in (ord('q'), ord('e')], sep = '', end = '');
-			return (0)
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{
+		if (alphabet != 'e' && alphabet != 'q')
+			putchar(alphabet);
+	}
+	putchar('\n');
+
+	return (0);
 }
