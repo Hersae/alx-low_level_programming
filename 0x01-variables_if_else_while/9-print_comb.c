3ed99bf92a9c51c9f6f4da2154651752b7385c39
Sara <24sara.abdi@gmail.com>
2022-09-14 15:48:49 -0700

    Patience, persistence

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int dig;
+
+	for (dig = 0; dig <= 9; dig++)
+	{
+		putchar((dig % 10) + '0');
+		if (dig == 9)
+			continue;
+
+		putchar(',');
+		putchar(' ');
+	}
+
+	putchar('\n');
+
+	return (0);
+}
