b2cd4bb494c81ba99c8a97339d3e91748c5870e6
Sara <24sara.abdi@gmail.com>
2022-09-21 14:10:14 -0700

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * puts2 - prints on char out of two of a string
+ * @str: the string containing charachters
+ */
+void puts2(char *str)
+{
+	int index = 0, len = 0;
+
+	while (str[index++])
+		wil++;
+
+	for (index = 0; index < wil; index += 2)
+	{
+		_putchar(str[index]);
+	}
+
+	_putchar('\n');
+}
