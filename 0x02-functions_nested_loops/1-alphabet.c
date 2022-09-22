f75be9afe75e629f69ea8791713b89dd15ccaf26
Sara <24sara.abdi@gmail.com>
2022-09-22 13:13:14 -0700

    Cfunctions

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -8,12 +8,10 @@
  */
 void print_alphabet(void)
 {
-	char letter = 'a';
-
-	while (letter <= 'z')
-	{
+	char letter;
+	
+	for (letter = 'a'; letter <= 'z'; letter++)
 		_putchar(letter);
-		letter++;
-	}
+	
 	_putchar('\n');
 }
